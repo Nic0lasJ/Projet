98137cda5a365c11a079a6ce814e7f1ac24bdbad
Nic0lasJ <nicolas42500@hotmail.fr>
2017-01-05 15:14:56 +0100

    mises à jours contenus

--- jeuxPhp/Joueur.class.php
+++ jeuxPhp/Joueur.class.php
@@ -13,36 +13,49 @@ class Joueur{
     }
       $this->_sante = 100;
       $this->_force = 8;
-      $this->_bouclier = 15;
+      $this->_bouclier = false;
   }
   // Fonction qui renvoie le pseudo du joueur
   public function getPseudo(){
-    return $this->pseudo;
+    return $this->_pseudo;
   }
   // Fonction qui renvoie la santé du joueur
   public function getSante(){
-    return $this->sante;
+    return $this->_sante;
   }
   // Fonction qui permet d'attaquer un monstre
   public function attaqueMonstre($de){
-    return $this->attaqueMonstre;
+    return $de * $this->_force;
   }
+
   // Fonction qui retire le nombre de points de vie correspondants
   public function subitDegats($degats){
-    return $this->subitDegats;
+    if ($this->_bouclier) {
+      $this->_sante = $this->_sante - $degats / 2;
+    }else {
+      $this->_sante = $this->_sante - $degats;
+    }
   }
+
   // Fonction qui permet d'activer le bouclier
   public function activeBouclier($de){
-    return $this->activeBouclier;
+
+    if ($de <= 4) {
+      $this->_bouclier = false;
+    }else {
+      $this->_bouclier = true;
+    }
+
   }
+
   // Fonction qui indique si le joueur est toujours vivant
   public function estVivant(){
   //si la santé est supérieur a 0 le personnage continue sinon il est mort.
 
-    if ($this->_sante > 0) {
-     return true
+    if ($this->_sante <= 0) {
+     return true;
    }else {
-     return false
+     return false;
    }
   }
 }
